6f77a8bbe15800c9154e346d2ae3fb87adf36eb2
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-20 15:48:41 +0530

    added routes and router

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import router from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", router);
 
 app.listen(PORT, () => {
   console.log(`server is runnig on port: ${PORT}`);
