9d85885a1d46502c4cd7458b1476110fd8f3e4fd
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-21 15:28:08 +0530

    generated token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,13 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
-    const { fullname, username, password, confirmPassword, gender } = req.body;
+    console.log("Hi I have signed up");
+
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    console.log(fullName);
 
     if (password !== confirmPassword) {
       return res.status(400).json({ error: "Passwords don't match" });
@@ -14,6 +19,13 @@ export const signup = async (req, res) => {
       return res.status(400).json({ error: "Username already exists" });
     }
 
+    // Hash the password
+
+    const salt = await bcrypt.genSalt(10);
+    console.log(salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log(hashedPassword);
+
     // for profile picture of user
     // https://avatar-placeholer.iran.liara.run
 
@@ -21,31 +33,66 @@ export const signup = async (req, res) => {
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
     const newUser = new User({
-      fullname,
+      fullName,
       username,
-      password,
+      password: hashedPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
+    if (newUser) {
+      // generate jwt token
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullname: newUser.fullname,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Failed to create user" });
+    }
   } catch (e) {
     console.log("Error in signup controller", e.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-export const login = (req, res) => {
-  res.send("login");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user) {
+      res.status(400).json({ error: "Invalid username" });
+    } else if (!isPasswordCorrect) {
+      res.status(400).json({ error: "Invalid password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+
+    console.log("User logged in");
+  } catch (e) {
+    console.log("Error in login controller", e.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
+  try {
+  } catch (e) {}
   res.send("logout");
 };
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  console.log("token generated");
+  res.cookie("jwt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV === "production", // only set cookie on production environment
+  });
+};
+
+export default generateTokenAndSetCookie;
