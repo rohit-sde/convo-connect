7166a619c0a964a102b26a8b4996fe67b6c95ac4
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-21 19:44:41 +0530

    message functionality added

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,37 @@
+import Conversation from "../models/conversation.model.js";
+
 export const sendMessage = async (res, req) => {
-  console.log(`Message sent Successfully :  ${res.params.id}`);
+  try {
+    const { message } = req.body;
+    const { id } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = new Conversation.findone({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    await conversation.save();
+    await newMessage.save();
+
+    res.status(201).json({ message: "Message sent successfully" });
+  } catch (err) {
+    console.log("Error in Send Message Controller: ", err.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import router from "./routes/auth.routes.js";
 import messageRouter from "./routes/message.routes.js";
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 8181;
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", router);
 app.use("/api/message", messageRouter);
