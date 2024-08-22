bd7f3937d5e8bfd06d27e9501c0c8fe7aae134b0
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-22 15:32:48 +0530

    signup page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
-import Home from "./pages/home/home.jsx";
-import Login from "./pages/login/login";
+// import Login from "./pages/login/login";
+// import Home from "./pages/home/home.jsx";
 import SignUp from "./pages/signup/signup";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
-      <Login />
       <SignUp />
     </div>
   );
