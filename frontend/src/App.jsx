ae002a42a54004e873cd63c4785eecfba1334fe3
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-22 11:23:08 +0530

    signup, home and login pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Home from "./pages/home/home.jsx";
+import Login from "./pages/login/login";
+import SignUp from "./pages/signup/signup";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Home />
+      <Login />
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
