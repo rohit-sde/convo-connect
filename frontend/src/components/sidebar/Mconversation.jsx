a6332103105eb6d7a6241046307d3a0907108307
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-24 20:23:00 +0530

    users list rendered

--- frontend/src/components/sidebar/Mconversation.jsx
+++ frontend/src/components/sidebar/Mconversation.jsx
@@ -1,22 +1,32 @@
-import { FaRegUserCircle } from "react-icons/fa";
+/* eslint-disable react/prop-types */
+import useConversation from "../../zustand/useConversation";
 
-const Conversation = () => {
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
+
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 p-2 py-1 cursor-pointer">
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 p-2 py-1 cursor-pointer ${
+          isSelected ? "bg-sky-500" : ""
+        }`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <FaRegUserCircle className="mt-3 ml-3" />
+            <img src={conversation.profilePic} alt="user Avatar" />
           </div>
         </div>
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">Jhon Doe</p>
-            <span className="text-xl">😎</span>
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
           </div>
         </div>
       </div>
-      <div className="divider my-0 py-0 h-1" />
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 };
