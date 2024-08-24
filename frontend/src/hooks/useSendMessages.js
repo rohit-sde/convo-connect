17e19c8072f90e5003afd874027621897fb0892f
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-24 20:23:41 +0530

    sendMessage hook added

---
+++ frontend/src/hooks/useSendMessages.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import useConversation from "../zustand/useConversation";
+import toast from "react-hot-toast";
+
+const useSendMessages = () => {
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  const sendMessage = async (messageText) => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `/api/message/send/${selectedConversation._id}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ message: messageText }),
+        }
+      );
+      const data = await response.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      setMessages([...messages, data]);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { sendMessage, loading };
+};
+
+export default useSendMessages;
