18c3a14cd60c11b9be48d27741f0c3982283b002
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-24 20:21:42 +0530

    conversation rendered as per user selected

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,26 @@
 // import React from "react";
 import Conversation from "./Mconversation.jsx";
+import useGetConversations from "../../hooks/useGetConversations.js";
+import getRandomEmoji from "../utils/emojis.js";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation, idx) => {
+        return (
+          <Conversation
+            key={conversation._id}
+            conversation={conversation}
+            lastIdx={idx === conversations.length - 1}
+            emoji={getRandomEmoji()}
+          />
+        );
+      })}
+      {loading ? (
+        <span className="loading loading-spinner mx-auto "></span>
+      ) : null}
     </div>
   );
 };
