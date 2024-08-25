67c22d0e3288dc3a6548e3b51d7189198fe44e8a
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-25 21:54:25 +0530

    cors issue fixed.

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:8181"],
+    origin: ["http://localhost:8181", "http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
