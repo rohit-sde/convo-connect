19ccec0c0c3344758a3a0d048298fdd3394d6993
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-25 14:39:46 +0530

    added scrolling event at our latest message for better expierience

--- frontend/src/components/Messages/Messages.jsx
+++ frontend/src/components/Messages/Messages.jsx
@@ -1,12 +1,19 @@
 import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeleton/MessageSkeleton";
+import { useEffect, useRef } from "react";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
   console.log(messages, messages.length);
   const noofskeletons = messages.length;
+  const lastMessageRef = useRef();
 
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 50);
+  }, [messages]);
   return (
     <div className="pl-4 flex-1 overflow-auto">
       {loading &&
@@ -16,7 +23,11 @@ const Messages = () => {
       {!loading &&
         noofskeletons > 0 &&
         messages.map((message) => {
-          return <Message key={message._id} message={message} />;
+          return (
+            <div key={message._id} ref={lastMessageRef}>
+              <Message message={message} />;
+            </div>
+          );
         })}
       {!loading && noofskeletons === 0 && (
         <div className="flex items-center justify-center h-full text-center text-gray-500">
