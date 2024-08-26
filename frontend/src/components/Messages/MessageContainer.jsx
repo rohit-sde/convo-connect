e173373c93d03f59c949a7f1f1ad7868ec813f9d
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-26 12:45:05 +0530

    set user name on first impression of home page

--- frontend/src/components/Messages/MessageContainer.jsx
+++ frontend/src/components/Messages/MessageContainer.jsx
@@ -4,6 +4,7 @@ import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import useConversation from "../../zustand/useConversation";
 import { useEffect } from "react";
+import { useAuthContext } from "../../context/AuthContext";
 // import { useEffect } from "react";
 
 const MessageContainer = () => {
@@ -39,10 +40,11 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg ms:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 🤞 Jhon Doe </p>
+        <p>Welcome 🤞 {authUser.fullName} </p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
