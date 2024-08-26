57fa545f9104625f25069e689a9dfa8f713dc737
Rohit Singh <rohitsinghpb4@gmail.com>
2024-08-26 20:34:54 +0530

    ready files for bundeled

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import router from "./routes/auth.routes.js";
 import messageRouter from "./routes/message.routes.js";
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 8181;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -20,6 +23,11 @@ app.use("/api/auth", router);
 app.use("/api/message", messageRouter);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
 // });
